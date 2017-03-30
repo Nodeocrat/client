4962e9cad9aff3db75af93dd07d7c98eb0a68a67
Ubuntu <ubuntu@ip-172-31-27-151.eu-west-2.compute.internal>
2017-03-30 15:52:48 +0000

    home page edit

--- react-app-dev/src/components/Home/Home.js
+++ react-app-dev/src/components/Home/Home.js
@@ -50,8 +50,8 @@ export default (props) => {
             </div>
             <div className="panel-body panel-text">
               <p>With experience in banking software and big-data, real-time analytics software and a number of personal projects, I have solid experience of working with different types of software in different programming languages on both a macro and micro scale across the full stack and software lifecycle. I have worked with plenty of well designed and badly designed software so have a thorough appreciation of the importance of good software design and the trade-offs between complexity, time constraints and future maintenance/enhancements. Also being from a heavy maths background, algorithms and cunning solutions to difficult problems are one of my specialties.</p>
-              <p>Technologies I am proficient with: Node.js, JavaScript (ES-2016), C++, MongoDB, Linux, AWS, HTML + CSS + Bootstrap, Nginx, Express.js, Passport, Npm, Git, Mocha and various APIs including Google, Facebook and Stripe.</p>
-              <p>Other technologies i have worked with: Java, Dart, PostgreSQL/SQL, AngularJS 2.0, React.js (currently learning)</p>
+              <p>Technologies I am proficient with: Node.js, React, JavaScript (ES-2016), C++, MongoDB, Linux, AWS, HTML + CSS + Bootstrap, Nginx, Express.js, Passport, Npm, Git, Mocha and various APIs including Google, Facebook and Stripe.</p>
+              <p>Other technologies i have worked with: Java, Dart, PostgreSQL/SQL, AngularJS 2.0</p>
             </div>
         </div>
         <div className="panel panel-default">
