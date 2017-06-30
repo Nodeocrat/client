932efd6ebd6eb25270ced121da1984b95a909d78
Ashley Phillips <ashley.phillips@nodeocrat.com>
2017-06-30 12:15:34 +0100

    Add git repos to Node Social app project

--- react-app-dev/src/components/Projects/NodeSocial/NodeSocialDescription.js
+++ react-app-dev/src/components/Projects/NodeSocial/NodeSocialDescription.js
@@ -8,6 +8,8 @@ export default props => (
     <div className={text.center}><i>ES6, Node, React, Redux, Socket.io, Express.js, Advanced CSS features</i></div>
     <br/>
     <p>A lobby with a chat room which has been integrated with <Link to="/projects/GameProj">Node Shooter</Link>. Players can choose to create new instances of this game, or join one. This project makes advanced use of data structures and design patterns with the aforementioned technologies. It has been built to be easily extensible, as there are many more features yet to be added.</p>
+    <div><strong>Client code repository: </strong><a href="https://github.com/Nodeocrat/client/tree/redux/react-app-dev/src/components/Projects/NodeSocial" target="_blank">https://github.com/Nodeocrat/client/tree/redux/react-app-dev/src/components/Projects/NodeSocial</a></div>
+    <div><strong>Server code repository: </strong><a href="https://github.com/Nodeocrat/backend/tree/redux/NodeSocial" target="_blank">https://github.com/Nodeocrat/backend/tree/redux/NodeSocial</a></div>
     <br/>
   </div>
 );
