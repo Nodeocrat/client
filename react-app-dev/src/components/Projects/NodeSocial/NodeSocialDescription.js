cbb01c0373d0b7a4d6c6394126b5ba1ec7255d3b
Ashley Phillips <ashley.phillips@nodeocrat.com>
2017-07-13 21:56:36 +0000

    description update

--- react-app-dev/src/components/Projects/NodeSocial/NodeSocialDescription.js
+++ react-app-dev/src/components/Projects/NodeSocial/NodeSocialDescription.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 export default props => (
   <div>
     <div className={`${text.title} ${text.center}`}>Node Social App</div>
-    <div className={text.center}><i>ES6, Node, React, Redux, Socket.io, Express.js, Advanced CSS features</i></div>
+    <div className={text.center}><i>ES6, Node, React, Redux, Socket.io, Express.js, CSS3 Flexbox model</i></div>
     <br/>
     <p>{"This is an online interactive lobby/multiplayer game room, so it'll be a little boring if you are testing it by yourself."}</p>
     <p>It has been integrated with <Link to="/projects/GameProj">Node Shooter</Link>. Players can choose to create new instances of this game, or join one. This project makes advanced use of data structures and design patterns with the aforementioned technologies. It has been built to be easily extensible, as there are many more features yet to be added.</p>
